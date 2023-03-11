9eeeb8979c51135ecb138cbfda31a08e7cfc6197
kozhatikoff <103827424+kozhatikoff@users.noreply.github.com>
2023-03-11 13:39:19 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+let quality=document.querySelector(".mainView")
+let quanity=document.querySelector(".percent")
+let people=document.querySelector(".peop")
+let button=document.querySelector(".score")
+let mainView=document.querySelector(".conc")
+let conclusion=document.querySelector(".conc")
+button.addEventListener("click",()=>{
+    if(quanity.value=="bad"){
+        
+
+    }
+
+
+})
